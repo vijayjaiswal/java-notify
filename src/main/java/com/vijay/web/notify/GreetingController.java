df390d6891b63d949b263b99c88022ba8a604ea5
Vijay Jaiswal <vijay.jaiswal@impetus.co.in>
2016-12-13 19:40:32 +0530

    Adding new files

---
+++ src/main/java/com/vijay/web/notify/GreetingController.java
@@ -0,0 +1,27 @@
+package com.vijay.web.notify;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello Welcome, %s! Current Time is: %s";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("/greeting")
+	public Greeting greeting(
+			@RequestParam(value = "name", defaultValue = "World") String name) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss z");
+		String date = sdf.format(new Date());
+		System.out.println(date);
+		
+		return new Greeting(counter.incrementAndGet(), String.format(template,
+				name,date));
+	}
+}
